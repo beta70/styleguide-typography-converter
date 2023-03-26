199461c01aa27f7b2d239c0f66478d6117a35fc7
Erik Wartenberg <erik@studiobosco.de>
2023-03-26 22:38:25 +0200

    extracted logic from main app to form row store

--- prototype-converter/src/App.vue
+++ prototype-converter/src/App.vue
@@ -3,135 +3,10 @@
   import Form from './components/Form.vue'
   import Tabs from './components/Tabs.vue'
   import { computed,ref } from 'vue'
+  import { useFormRowStore } from '../store/formRowStore'
 
-  const formRows = ref([
-    {
-      id: Math.ceil(Math.random() * 1000000),
-      fields: [
-        { 
-          name: 'font-size',
-          fieldType: 'input',
-          placeholder: '16px',
-          dataType: 'number',
-          allowedUnits: ['px','rem','em'],
-          conversionOptions: ['rem','vw','clamp()'],
-          range: true,
-          targetUnit: '',
-          givenUnit: '',
-          value: {
-            min: '',
-            max: '',
-          },
-        },
-        { 
-          name: 'line-height',
-          fieldType: 'input',
-          dataType: 'number',
-          placeholder: '24px',
-          allowedUnits: ['px','rem','em','num'],
-          conversionOptions: ['num','rem'],
-          targetUnit: '',
-          givenUnit: '',
-          value: '',
-        },
-        { 
-          name: 'letter-spacing',
-          fieldType: 'input',
-          dataType: 'number',
-          placeholder: '1px',
-          allowedUnits: ['px','rem','em'],
-          conversionOptions: ['em','rem'],
-          targetUnit: '',
-          givenUnit: '',
-          value: '',
-        },
-        { 
-          name: 'font-weight',
-          fieldType: 'input',
-          placeholder: '400',
-          dataType: 'number',
-          value: '',
-        },
-        { 
-          name: 'color',
-          fieldType: 'input',
-          placeholder: '#ACACAC',
-          dataType: 'string',
-          value: '',
-        },
-      ]
-    }
-  ])
-  function addFormRow() {
-    formRows.value.push(
-      {
-        id: Math.ceil(Math.random() * 1000000),
-        fields: [
-          { 
-            name: 'font-size',
-            fieldType: 'input',
-            placeholder: '16px',
-            dataType: 'number',
-            allowedUnits: ['px','rem','em'],
-            conversionOptions: ['rem','vw','clamp()'],
-            range: true,
-            targetUnit: '',
-            givenUnit: '',
-            value: {
-              min: '',
-              max: '',
-            },
-          },
-          { 
-            name: 'line-height',
-            fieldType: 'input',
-            dataType: 'number',
-            placeholder: '24px',
-            allowedUnits: ['px','rem','em','num'],
-            conversionOptions: ['num','rem'],
-            targetUnit: '',
-            givenUnit: '',
-            value: '',
-          },
-          { 
-            name: 'letter-spacing',
-            fieldType: 'input',
-            dataType: 'number',
-            placeholder: '1px',
-            allowedUnits: ['px','rem','em'],
-            conversionOptions: ['em','rem'],
-            targetUnit: '',
-            givenUnit: '',
-            value: '',
-          },
-          { 
-            name: 'font-weight',
-            fieldType: 'input',
-            placeholder: '400',
-            dataType: 'number',
-            value: '',
-          },
-          { 
-            name: 'color',
-            fieldType: 'input',
-            placeholder: '#ACACAC',
-            dataType: 'string',
-            value: '',
-          },
-        ]
-      }
-    )
-  }
-  function deleteFormRow(id) {
-    formRows.value = formRows.value.filter(formRow => formRow.id !== id)
-    styleData.value = styleData.value.filter(data => data.id !== id)
-  }
-  const fieldData = computed(() => {
-    return formRows
-  })
-  const styleData = computed(() => {
-    return formRows
-  })
+  const store = useFormRowStore()
+
 
 
   const baseFontSize = ref('')
@@ -141,34 +16,6 @@
     if (property === 'max screen-width') maxScreenWidth.value = `${inputValue}px`
   }
   
-  function handleInputData(property,inputValue,id,key = 'value') {
-    const matchingFormRow = formRows.value.filter(formRow => formRow.id === id)[0]
-    const matchingField = matchingFormRow?.fields?.filter(field => field.name === property)[0]
-    let givenUnit
-
-    if (key === 'value') {
-      givenUnit = inputValue.match(/\D/g)?.join('')?.trim()
-      givenUnit = matchingField?.allowedUnits?.includes(givenUnit) ? givenUnit : matchingField?.allowedUnits?.[0]
-      matchingField['givenUnit'] = givenUnit
-      inputValue = matchingField.dataType === 'string' ? inputValue : inputValue.replace(/\D/g,'')
-      matchingField[key] = inputValue
-      return
-    }
-
-    if (key === 'min' || key === 'max') {
-      givenUnit = inputValue?.match(/\D/g)?.join('')?.trim()
-      givenUnit = matchingField?.allowedUnits?.includes(givenUnit) ? givenUnit : matchingField?.allowedUnits?.[0]
-      matchingField['givenUnit'] = givenUnit
-      inputValue = matchingField.dataType === 'string' ? inputValue : inputValue?.replace(/\D/g,'')
-      matchingField['value'][key] = inputValue
-      return
-    }
-
-    matchingField[key] = inputValue
-  }
-  function handleFontSizeInput(range,property,value,id) {
-    handleInputData(property,value,id,range)
-  }
   function handleConversionOptions(property,value,id) {
     handleInputData(property,value,id,'targetUnit')
   }
@@ -183,28 +30,24 @@
   <div>
       <Form 
         v-bind:form-rows="formRows" 
+        :formRowId="store.formRowId" 
         :base-font-size="baseFontSize"
         :max-screen-width="maxScreenWidth"
-        @handle-input-data="handleInputData"
-        @handle-conversion-options="handleConversionOptions"
-        @handle-font-size-input="handleFontSizeInput" 
         @handle-global-input-data="handleGlobalInputData"
-        @add-form-row="addFormRow"
-        @delete-form-row="deleteFormRow"
+        @delete-form-row="store.deleteFormRow()"
         @save-form-data="saveFormData"
 
         />
         <div class="flex justify-around">
           <pre class="text-black">
             <code>
-              {{ fieldData }}
+              {{ store.fieldData }}
               {{ baseFontSize }}
               {{ maxScreenWidth }}
             </code>
           </pre>
           <pre class="text-black">
             <code>
-              {{ styleData }}
             </code>
           </pre>
         </div>
